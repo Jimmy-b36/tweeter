426887555e1116b1979f6009618ac73d8801b9ea
James Ball <jamie.j.ball@gmail.com>
2022-05-25 14:51:38 -0700

    moved toggle func nto this file

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
-  const $textInput = $(this).find('textarea')
-  $textInput.on('input', () => {
-    let counter = $(this).find('output')
-    counter.val(140 - $textInput.val().length);
-  });
-
+  charCounter()
   charCounterWarnings()
+  toggleTextArea()
 });
 
+const charCounter = () => {
+  const $textInput = $(document).find('textarea')
+  $textInput.on('input', () => {
+    let counter = $(document).find('output')
+    counter.val(140 - $textInput.val().length);
+  });
+}
 
 const charCounterWarnings = () => {
   $('#tweet-text').on('keydown keyup change', function (event) {
@@ -26,4 +29,11 @@ const charCounterWarnings = () => {
       event.preventDefault();
     }
   });
+}
+
+// function to show and hide input area
+const toggleTextArea = () => {
+  $('#show-input').on('click', () => {
+    $('main section').first().slideToggle();
+  })
 }
